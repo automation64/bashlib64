bd4fe67d8150abd4c5186d47dc28fa634034fb3a
SerDigital64 <serdigital64@gmail.com>
2021-12-07 22:30:10 -0300

    skel: Update template

--- skel/generic.sh
+++ skel/generic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################
-# X_CODE_PURPOSE_X
+# X_APP_INFO_X
 #
 # Globals:
 #   None
@@ -15,28 +15,31 @@
 # Author: X_AUTHOR_ALIAS_X (X_AUTHOR_GIT_URL_X)
 # License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: X_PROJECT_GIT_URL_X
-# Version: X_CODE_VERSION_X
+# Version: X_APP_VERSION_X
 #######################################
 
 #
 # Imports
 #
 
+# shellcheck disable=SC2034
+SHELL64_LIB_STRICT='1'
 SHELL64_LIB='X_SHELL64_LIB_LOCATION_X'
-source "${SHELL64_LIB}/shell64.bash"
+# shellcheck disable=SC1091
+source "${SHELL64_LIB}/shell64.bash" || exit 1
 
 #
 # Exports
 #
 
-declare -xr X_NAMESPACE_SCRIPT_X_X_EXPORT_RO_X=''
-declare -x X_NAMESPACE_SCRIPT_X_X_EXPORT_X=''
+declare -xr X_APP_NAMESPACE_X_X_EXPORT_RO_X=''
+declare -x X_APP_NAMESPACE_X_X_EXPORT_X=''
 
 #
 # Functions
 #
 
-function X_NAMESPACE_SCRIPT_X_X_FUNCTION_COMMAND_X() {
+function X_APP_NAMESPACE_X_X_FUNCTION_COMMAND_X() {
 
   local flag="$1"
   local option="$2"
@@ -46,20 +49,7 @@ function X_NAMESPACE_SCRIPT_X_X_FUNCTION_COMMAND_X() {
 
 }
 
-#######################################
-# Check requirements and prerequisites
-#
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   Failed check description
-# Returns:
-#   0: check ok
-#   1: requirements not met
-#######################################
-function X_NAMESPACE_SCRIPT_X_check() {
+function X_APP_NAMESPACE_X_check() {
 
   local result=1
 
@@ -67,23 +57,11 @@ function X_NAMESPACE_SCRIPT_X_check() {
 
 }
 
-#######################################
-# Show script usage description
-#
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   Command line format and description
-# Returns:
-#   0
-#######################################
-function X_NAMESPACE_SCRIPT_X_help() {
+function X_APP_NAMESPACE_X_help() {
 
   shell64_msg_show_usage \
     'X_CODE_PURPOSE_CLI_X' \
-    'X_CODE_PURPOSE_X' \
+    'X_APP_INFO_X' \
     'X_CODE_PURPOSE_COMMANDSX' \
     'X_CODE_PURPOSE_FLAGSX' \
     'X_CODE_PURPOSE_PARAMETERSX'
@@ -94,37 +72,37 @@ function X_NAMESPACE_SCRIPT_X_help() {
 # Main
 #
 
-declare X_NAMESPACE_SCRIPT_X_status=1
-declare X_NAMESPACE_SCRIPT_X_command=''
-declare X_NAMESPACE_SCRIPT_X_X_OPTION_X=''
-declare X_NAMESPACE_SCRIPT_X_X_FLAG_X='0'
-declare X_NAMESPACE_SCRIPT_X_option=''
+declare X_APP_NAMESPACE_X_status=1
+declare X_APP_NAMESPACE_X_command=''
+declare X_APP_NAMESPACE_X_X_OPTION_X=''
+declare X_APP_NAMESPACE_X_X_FLAG_X='0'
+declare X_APP_NAMESPACE_X_option=''
 
-(( $# == 0 )) && X_NAMESPACE_SCRIPT_X_help && exit 1
-while getopts ':xy:zh' X_NAMESPACE_SCRIPT_X_option; do
-  case "$X_NAMESPACE_SCRIPT_X_option" in
+(( $# == 0 )) && X_APP_NAMESPACE_X_help && exit 1
+while getopts ':xy:zh' X_APP_NAMESPACE_X_option; do
+  case "$X_APP_NAMESPACE_X_option" in
   x)
-    X_NAMESPACE_SCRIPT_X_command='X_NAMESPACE_SCRIPT_X_X_FUNCTION_COMMAND_X'
+    X_APP_NAMESPACE_X_command='X_APP_NAMESPACE_X_X_FUNCTION_COMMAND_X'
     ;;
   y)
-    X_NAMESPACE_SCRIPT_X_X_OPTION_X="$OPTARG"
+    X_APP_NAMESPACE_X_X_OPTION_X="$OPTARG"
     ;;
   z)
-    X_NAMESPACE_SCRIPT_X_X_FLAG_X='1'
+    X_APP_NAMESPACE_X_X_FLAG_X='1'
     ;;
   h)
-    X_NAMESPACE_SCRIPT_X_help && exit
+    X_APP_NAMESPACE_X_help && exit
     ;;
   \?)
-    X_NAMESPACE_SCRIPT_X_help && exit 1
+    X_APP_NAMESPACE_X_help && exit 1
     ;;
   esac
 done
-[[ -z "$X_NAMESPACE_SCRIPT_X_command" ]] && X_NAMESPACE_SCRIPT_X_help && exit 1
-[[ -z "$X_NAMESPACE_SCRIPT_X_X_OPTION_X" ]] && X_NAMESPACE_SCRIPT_X_help && exit 1
-X_NAMESPACE_SCRIPT_X_check || exit 1
+[[ -z "$X_APP_NAMESPACE_X_command" ]] && X_APP_NAMESPACE_X_help && exit 1
+[[ -z "$X_APP_NAMESPACE_X_X_OPTION_X" ]] && X_APP_NAMESPACE_X_help && exit 1
+X_APP_NAMESPACE_X_check || exit 1
 
-"$X_NAMESPACE_SCRIPT_X_command" "$X_NAMESPACE_SCRIPT_X_X_FLAG_X" "$X_NAMESPACE_SCRIPT_X_X_OPTION_X"
+"$X_APP_NAMESPACE_X_command" "$X_APP_NAMESPACE_X_X_FLAG_X" "$X_APP_NAMESPACE_X_X_OPTION_X"
 
-X_NAMESPACE_SCRIPT_X_status=$?
-exit $X_NAMESPACE_SCRIPT_X_status
+X_APP_NAMESPACE_X_status=$?
+exit $X_APP_NAMESPACE_X_status
