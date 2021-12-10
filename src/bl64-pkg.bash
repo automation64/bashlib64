c5dd7ad81d529ca5312c940ed3e2bead464cecb6
SerDigital64 <serdigital64@gmail.com>
2021-12-10 18:39:09 -0300

    pkg: new

---
+++ src/bl64-pkg.bash
@@ -0,0 +1,32 @@
+#######################################
+# BashLib64 / Install native OS packages
+#
+# Author: serdigital64 (https://github.com/serdigital64)
+# License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
+# Repository: https://github.com/serdigital64/bashlib64
+# Version: 1.0.0
+#######################################
+
+#
+# Internal Functions
+#
+
+#
+# Exported Functions
+#
+
+#######################################
+# Initialize the package manager for installations
+#
+# Globals:
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: None
+#   STDERR: Error message
+# Returns:
+#   0: 
+#######################################
+function bl64_pkg_prepare() {
+
+}
