6f15dbb632e8bf674c79a49e6fa2de00ecee12b8
SerDigital64 <serdigital64@gmail.com>
2021-12-09 21:20:06 -0300

    build: update to new structure

--- bin/build-prod.bash
+++ bin/build-prod.bash
@@ -2,15 +2,15 @@
 
 . ./.env || exit 1
 
-project_bl64_modules='log msg os'
+project_bl64_modules='log msg os core'
 project_bl64_file=''
 project_bl64_lib="$PROJECT_BL64_BUILD/bashlib64.bash"
 
-cat 'PROJECT_BL64_SRC/bashlib64-core.bash' > "$project_bl64_lib"
+cat "$PROJECT_BL64_SRC/bashlib64-core.header" > "$project_bl64_lib"
 for project_bl64_file in $project_bl64_modules; do
-  grep -v -e '^#.*' "${PROJECT_BL64_SRC}/bashlib64-${project_bl64_file}.env" >> "$project_bl64_lib"
+  grep -v -E '^#.*|^$' "${PROJECT_BL64_SRC}/bashlib64-${project_bl64_file}.env" >> "$project_bl64_lib"
 done
 
 for project_bl64_file in $project_bl64_modules; do
-  grep -v -e '^#.*' "${PROJECT_BL64_SRC}/bashlib64-${project_bl64_file}.bash" >> "$project_bl64_lib"
+  grep -v -E '^#.*|^$' "${PROJECT_BL64_SRC}/bashlib64-${project_bl64_file}.bash" >> "$project_bl64_lib"
 done
