3df4353a3f6f2c0c7012c3f38381d02a52ba0147
SerDigital64 <serdigital64@gmail.com>
2021-12-11 12:29:35 -0300

    fmt: add

---
+++ src/bl64-fmt.bash
@@ -0,0 +1,36 @@
+#######################################
+# BashLib64 / Format data
+#
+# Author: serdigital64 (https://github.com/serdigital64)
+# License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
+# Repository: https://github.com/serdigital64/bashlib64
+# Version: 1.0.0
+#######################################
+
+#
+# Internal Functions
+#
+
+#
+# Exported Functions
+#
+
+#######################################
+# Check and report if the command is present and has execute permissions for the current user.
+#
+# Globals:
+#   BL64_OS_CMD_GREP
+# Arguments:
+#   $1: Full path to the command to check
+# Outputs:
+#   STDOUT: None
+#   STDERR: Error message
+# Returns:
+#   grep command exit status
+#######################################
+function bl64_fmt_strip_comments() {
+  local source="${1:--}"
+
+  "$BL64_OS_CMD_GREP" -v -E '^#.*$|^ *#.*$' "$source"
+
+}
