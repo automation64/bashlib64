f98a3d8dcca58adc407bd1071d8fb5ab733564b8
SerDigital64 <serdigital64@gmail.com>
2021-12-11 09:42:26 -0300

    os: add cleanup

--- src/bl64-os.bash
+++ src/bl64-os.bash
@@ -4,7 +4,7 @@
 # Author: serdigital64 (https://github.com/serdigital64)
 # License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: https://github.com/serdigital64/bashlib64
-# Version: 1.0.4
+# Version: 1.1.0
 #######################################
 
 #
@@ -129,10 +129,93 @@ function bl64_os_set_command() {
 #######################################
 function bl64_os_set_alias() {
 
-  BL64_OS_ALIAS_LS_FILES="$BL64_OS_CMD_LS --color=never"
-  BL64_OS_ALIAS_ID_USER="$BL64_OS_CMD_ID -u -n"
-  BL64_OS_ALIAS_CP_FILE="$BL64_OS_CMD_CP --verbose --force"
   BL64_OS_ALIAS_CHOWN_DIR="$BL64_OS_CMD_CHOWN --verbose --recursive"
+  BL64_OS_ALIAS_CP_FILE="$BL64_OS_CMD_CP --verbose --force"
+  BL64_OS_ALIAS_ID_USER="$BL64_OS_CMD_ID -u -n"
+  BL64_OS_ALIAS_LS_FILES="$BL64_OS_CMD_LS --color=never"
+  BL64_OS_ALIAS_RM_FILE="$BL64_OS_CMD_CP --verbose --force --one-file-system"
+  BL64_OS_ALIAS_RM_FULL="$BL64_OS_CMD_CP --verbose --force --one-file-system --recursive"
   BL64_OS_ALIAS_SUDO_ENV="$BL64_OS_CMD_SUDO --preserve-env --set-home"
+
+}
+
+#######################################
+# Remove content from OS temporary repositories
+#
+# Globals:
+#   BL64_OS_ALIAS_RM_FILE
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: output from the rm command
+#   STDERR: output from the rm command
+# Returns:
+#   0: always ok
+#######################################
+function bl64_os_cleanup_tmps() {
+
+  $BL64_OS_ALIAS_RM_FILE -- /tmp/*
+  $BL64_OS_ALIAS_RM_FILE -- /var/tmp/*
+  :
+
+}
+
+#######################################
+# Remove or reset logs from standard locations
+#
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: None
+#   STDERR: None
+# Returns:
+#   0: always ok
+#######################################
+function bl64_os_cleanup_logs() {
+
+  :
+
+}
+
+#######################################
+# Remove or reset caches from standard locations
+#
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: None
+#   STDERR: None
+# Returns:
+#   0: always ok
+#######################################
+function bl64_os_cleanup_caches() {
+
+  :
+
+}
+
+#######################################
+# Removes temporary content, resets logs and caches from standard locations
+#
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: output from clean functions
+#   STDERR: output from clean functions
+# Returns:
+#   0: always ok
+#######################################
+function bl64_os_cleanup_full() {
+
+  bl64_os_cleanup_tmps
+  bl64_os_cleanup_logs
+  bl64_os_cleanup_caches
+  :
 
 }
