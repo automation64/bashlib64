d11f123ecce4c2ccc22201859ba437de7a92e5e0
SerDigital64 <serdigital64@gmail.com>
2021-12-11 20:42:13 -0300

    pkg: add missing package list

--- src/bl64-pkg.bash
+++ src/bl64-pkg.bash
@@ -4,7 +4,7 @@
 # Author: serdigital64 (https://github.com/serdigital64)
 # License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: https://github.com/serdigital64/bashlib64
-# Version: 1.0.0
+# Version: 1.0.1
 #######################################
 
 #######################################
@@ -44,7 +44,7 @@ function bl64_pkg_prepare() {
 # Globals:
 #   BL64_OS_DISTRO
 # Arguments:
-#   None
+#   package list, separated by spaces (expanded with $@)
 # Outputs:
 #   STDOUT: None
 #   STDERR: error from package manager
@@ -58,10 +58,10 @@ function bl64_pkg_install() {
   case "$BL64_OS_DISTRO" in
   UBUNTU-* | DEBIAN-*)
     export DEBIAN_FRONTEND="noninteractive"
-    $BL64_PKG_ALIAS_APT_INSTALL
+    $BL64_PKG_ALIAS_APT_INSTALL -- "$@"
     ;;
   FEDORA-* | CENTOS-* | OL-*)
-    $BL64_PKG_ALIAS_DNF_INSTALL
+    $BL64_PKG_ALIAS_DNF_INSTALL -- "$@"
     ;;
   esac
 
