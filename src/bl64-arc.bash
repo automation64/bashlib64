4ef2a470b8b430fdefd061f3de240785fe08c00b
SerDigital64 <serdigital64@gmail.com>
2021-12-11 17:02:23 -0300

    arc: add open tar

--- src/bl64-arc.bash
+++ src/bl64-arc.bash
@@ -8,20 +8,62 @@
 #######################################
 
 #######################################
-# Open TGZ file and remove source
+# Open tar files and remove the source after extraction
+#
+# * Preserves permissions but not ownership
+# * Overwrites destination
 #
 # Globals:
 #   BL64_OS_CMD_TAR
+#   BL64_OS_ALIAS_RM_FILE
+#   _BL64_ARC_TXT_MISSING_PARAMETER
+#   _BL64_ARC_TXT_DST_NOT_DIRECTORY
+#   BL64_ERROR_MISSING_PARAMETER
+#   BL64_ERROR_INVALID_DESTINATION
 # Arguments:
 #   $1: Full path to the source file
+#   $2: Full path to the destination
 # Outputs:
 #   STDOUT: None
-#   STDERR: tar error messages
+#   STDERR: tar or lib error messages
 # Returns:
+#   BL64_ERROR_MISSING_PARAMETER
+#   BL64_ERROR_INVALID_DESTINATION
 #   tar error status
 #######################################
-function bl64_arc_open_tgz() {
+function bl64_arc_open_tar() {
+  local source="$1"
+  local destination="$2"
+  local status=0
 
-  :
+  if [[ -z "$source" || -z "$destination" ]]; then
+    bl64_msg_show_error "$_BL64_ARC_TXT_MISSING_PARAMETER (source,destination)"
+    # shellcheck disable=SC2086
+    return $BL64_ARC_ERROR_MISSING_PARAMETER
+  fi
+
+  if [[ ! -d "$destination" ]]; then
+    bl64_msg_show_error "$_BL64_ARC_TXT_DST_NOT_DIRECTORY ($destination)"
+    # shellcheck disable=SC2086
+    return $BL64_ARC_ERROR_INVALID_DESTINATION
+  fi
+
+  cd "$destination" || return 1
+
+  "$BL64_OS_CMD_TAR" \
+    --overwrite \
+    --extract \
+    --no-same-owner \
+    --preserve-permissions \
+    --no-acls \
+    --force-local \
+    --verbose \
+    --auto-compress \
+    --file="$source"
+  status=$?
+
+  ((status == 0)) && $BL64_OS_ALIAS_RM_FILE "$source"
+
+  return $status
 
 }
