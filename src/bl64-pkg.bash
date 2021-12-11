018ad1ab0fb95d08cfa5f1c3b5faf9ce9eeac9b3
SerDigital64 <serdigital64@gmail.com>
2021-12-11 09:07:53 -0300

    pkd: add

--- src/bl64-pkg.bash
+++ src/bl64-pkg.bash
@@ -25,8 +25,76 @@
 #   STDOUT: None
 #   STDERR: Error message
 # Returns:
-#   0: 
+#   0:
 #######################################
 function bl64_pkg_prepare() {
+
+  export LC_ALL="C"
+
+  case "$BL64_OS_DISTRO" in
+  UBUNTU-* | DEBIAN-*)
+    export DEBIAN_FRONTEND="noninteractive"
+    $BL64_PKG_ALIAS_APT_UPDATE
+    ;;
+  FEDORA-* | CENTOS-* | OL-*)
+    $BL64_PKG_ALIAS_DNF_CACHE
+    ;;
+  esac
+
+}
+
+#######################################
+# Install packages
+#
+# Globals:
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: None
+#   STDERR: Error message
+# Returns:
+#   0:
+#######################################
+function bl64_pkg_install() {
+
+  export LC_ALL="C"
+
+  case "$BL64_OS_DISTRO" in
+  UBUNTU-* | DEBIAN-*)
+    export DEBIAN_FRONTEND="noninteractive"
+    $BL64_PKG_ALIAS_APT_INSTALL
+    ;;
+  FEDORA-* | CENTOS-* | OL-*)
+    $BL64_PKG_ALIAS_DNF_INSTALL
+    ;;
+  esac
+
+}
+
+#######################################
+# Clean up the package manager run-time environment
+#
+# Globals:
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: None
+#   STDERR: Error message
+# Returns:
+#   0:
+#######################################
+function bl64_pkg_cleanup() {
+
+  export LC_ALL="C"
+
+  case "$BL64_OS_DISTRO" in
+  UBUNTU-* | DEBIAN-*)
+    export DEBIAN_FRONTEND="noninteractive"
+    $BL64_PKG_ALIAS_APT_CLEAN
+    ;;
+  FEDORA-* | CENTOS-* | OL-*)
+    $BL64_PKG_ALIAS_DNF_CLEAN
+    ;;
+  esac
 
 }
