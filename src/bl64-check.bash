95763846685ac9832f69f021276890897ddd455d
SerDigital64 <serdigital64@gmail.com>
2022-01-23 12:00:39 -0300

    feat: add param check

--- src/bl64-check.bash
+++ src/bl64-check.bash
@@ -4,7 +4,7 @@
 # Author: serdigital64 (https://github.com/serdigital64)
 # License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: https://github.com/serdigital64/bashlib64
-# Version: 1.1.0
+# Version: 1.2.0
 #######################################
 
 #######################################
@@ -108,6 +108,39 @@ function bl64_check_directory() {
     bl64_msg_show_error "$_BL64_CHECK_TXT_DIRECTORY_NOT_READABLE ($path)"
     # shellcheck disable=SC2086
     return $BL64_CHECK_ERROR_DIRECTORY_NOT_READ
+  fi
+  :
+}
+
+#######################################
+# Check shell parameters:
+#   - parameter is not empty
+#
+# Arguments:
+#   $1: parameter name
+#   $2: parameter description. Shown on error messages
+# Outputs:
+#   STDOUT: None
+#   STDERR: Error message
+# Returns:
+#   0: Check ok
+#   $BL64_CHECK_ERROR_MISSING_PARAMETER
+#   $BL64_CHECK_ERROR_PARAMETER_EMPTY
+#######################################
+function bl64_check_parameter() {
+  local parameter="$1"
+  local description="${2:-parameter $parameter}"
+
+  if [[ -z "$parameter" ]]; then
+    bl64_msg_show_error "$_BL64_CHECK_TXT_MISSING_PARAMETER (parameter name)"
+    # shellcheck disable=SC2086
+    return $BL64_CHECK_ERROR_MISSING_PARAMETER
+  fi
+
+  if eval "[[ -z \$${parameter} ]]"; then
+    bl64_msg_show_error "$_BL64_CHECK_TXT_MISSING_PARAMETER ($description)"
+    # shellcheck disable=SC2086
+    return $BL64_CHECK_ERROR_PARAMETER_EMPTY
   fi
   :
 }
