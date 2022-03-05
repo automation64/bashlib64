7f67bc1d007dc041aa3c3aca3cf44567d8e64c44
serdigital64 <serdigital64@gmail.com>
2022-03-04 21:02:22 -0300

    feat: add module txt

---
+++ src/bl64-txt.bash
@@ -0,0 +1,28 @@
+#######################################
+# BashLib64 / Manipulate text files content
+#
+# Author: serdigital64 (https://github.com/serdigital64)
+# License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
+# Repository: https://github.com/serdigital64/bashlib64
+# Version: 1.0.0
+#######################################
+
+#######################################
+# Search for a whole line in a given text file
+#
+# Arguments:
+#   $1: source file path
+#   $2: text to look for
+# Outputs:
+#   STDOUT: none
+#   STDERR: Error messages
+# Returns:
+#   0: line was found
+#   >0: grep command exit status
+#######################################
+function bl64_txt_search_line() {
+  local source="${1:--}"
+  local line="$2"
+
+  "$BL64_OS_CMD_GREP" -E "^${line}$" "$source" > /dev/null
+}
