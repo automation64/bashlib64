121d20b3befa93cb63cd82100cf603a840f2125c
serdigital64 <serdigital64@gmail.com>
2022-03-25 10:54:12 -0300

    feat: add module dbg

--- src/bl64-dbg.bash
+++ src/bl64-dbg.bash
@@ -6,3 +6,50 @@
 # Repository: https://github.com/serdigital64/bashlib64
 # Version: 1.0.0
 #######################################
+
+#######################################
+# Enable shell function tracing
+#
+# Arguments:
+#   $1: function name
+# Outputs:
+#   STDOUT: Tracing
+#   STDERR: Debug messages
+# Returns:
+#   0: always ok
+#######################################
+function bl64_dbg_function_start() { set +x
+  local name="$1"
+
+  [[ "$BL64_LIB_DEBUG" != "$BL64_DBG_TARGET_FNC" ]] && return
+  [[ "${FUNCNAME[1]}" != "$name" ]] && return
+
+  bl64_msg_show_debug "[${name}] start tracing function"
+  bl64_msg_show_debug "[${name}] function source: line:${BASH_LINENO[1]}@file:${BASH_SOURCE[1]}"
+  bl64_msg_show_debug "[${name}] function caller: ${FUNCNAME[2]}@line:${BASH_LINENO[2]}@file:${BASH_SOURCE[2]}"
+  set -x
+
+  return
+}
+
+#######################################
+# Disable shell function tracing
+#
+# Arguments:
+#   $1: function name
+# Outputs:
+#   STDOUT: Tracing
+#   STDERR: Debug messages
+# Returns:
+#   0: always ok
+#######################################
+function bl64_dbg_function_stop() {
+  local name="$1"
+
+  [[ "$BL64_LIB_DEBUG" != "$BL64_DBG_TARGET_FNC" ]] && return
+  [[ "${FUNCNAME[1]}" != "$name" ]] && return
+  set +x
+  bl64_msg_show_debug "[${name}] stop tracing function"
+
+  return
+}
