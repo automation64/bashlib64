4a137be4dad30347fbbdf01040069d5b47fd565c
serdigital64 <serdigital64@gmail.com>
2022-04-16 17:39:27 -0300

    feat: add unsuported msg

--- src/bl64-msg.bash
+++ src/bl64-msg.bash
@@ -4,7 +4,7 @@
 # Author: serdigital64 (https://github.com/serdigital64)
 # License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: https://github.com/serdigital64/bashlib64
-# Version: 1.5.0
+# Version: 1.6.0
 #######################################
 
 #######################################
@@ -297,3 +297,20 @@ function bl64_msg_show_batch_finish() {
   fi
 }
 
+#######################################
+# Display unsupported platform message
+#
+# Arguments:
+#   $1: target (function name, command path, etc)
+# Outputs:
+#   STDOUT: none
+#   STDERR: message
+# Returns:
+#   0: successfull execution
+#   >0: printf error
+#######################################
+function bl64_msg_show_unsupported() {
+  local target="${1:-"${FUNCNAME[1]}"}"
+
+  bl64_msg_show_error "${_BL64_MSG_TXT_INCOMPATIBLE} (os: ${BL64_OS_DISTRO} / target: ${target})"
+}
