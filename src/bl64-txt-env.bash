521c466f18d25a0d6ae6671e457805f43e96121d
serdigital64 <serdigital64@gmail.com>
2022-05-08 16:40:13 -0300

    feat: add set,tr,base64

--- src/bl64-txt-env.bash
+++ src/bl64-txt-env.bash
@@ -7,3 +7,6 @@
 export BL64_TXT_CMD_AWK=''
 export BL64_TXT_CMD_GAWK=''
 export BL64_TXT_CMD_GREP=''
+export BL64_TXT_CMD_SED=''
+export BL64_TXT_CMD_TR=''
+export BL64_TXT_CMD_BASE64=''
--- src/bl64-txt-init.bash
+++ src/bl64-txt-init.bash
@@ -24,21 +24,33 @@ function bl64_txt_set_command() {
     BL64_TXT_CMD_AWK='/usr/bin/awk'
     BL64_TXT_CMD_GAWK='/usr/bin/gawk'
     BL64_TXT_CMD_GREP='/bin/grep'
+    BL64_TXT_CMD_SED='/bin/sed'
+    BL64_TXT_CMD_TR='/usr/bin/tr'
+    BL64_TXT_CMD_BASE64='/usr/bin/base64'
     ;;
   ${BL64_OS_FD}-* | ${BL64_OS_CNT}-* | ${BL64_OS_RHEL}-* | ${BL64_OS_ALM}-* | ${BL64_OS_OL}-*)
     BL64_TXT_CMD_AWK='/usr/bin/awk'
     BL64_TXT_CMD_GAWK='/usr/bin/gawk'
     BL64_TXT_CMD_GREP='/usr/bin/grep'
+    BL64_TXT_CMD_SED='/usr/bin/sed'
+    BL64_TXT_CMD_TR='/usr/bin/tr'
+    BL64_TXT_CMD_BASE64='/usr/bin/base64'
     ;;
   ${BL64_OS_ALP}-*)
     BL64_TXT_CMD_AWK='/usr/bin/awk'
     BL64_TXT_CMD_GAWK='/usr/bin/gawk'
     BL64_TXT_CMD_GREP='/bin/grep'
+    BL64_TXT_CMD_SED='/bin/sed'
+    BL64_TXT_CMD_TR='/usr/bin/tr'
+    BL64_TXT_CMD_BASE64='/bin/base64'
     ;;
   ${BL64_OS_MCOS}-*)
     BL64_TXT_CMD_AWK='/usr/bin/awk'
     BL64_TXT_CMD_GAWK="$BL64_LIB_INCOMPATIBLE"
     BL64_TXT_CMD_GREP='/usr/bin/grep'
+    BL64_TXT_CMD_SED='/usr/bin/sed'
+    BL64_TXT_CMD_TR='/usr/bin/tr'
+    BL64_TXT_CMD_BASE64='/usr/bin/base64'
     ;;
   *) bl64_check_show_unsupported ;;
   esac
