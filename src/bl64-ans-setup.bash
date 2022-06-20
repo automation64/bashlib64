f4c045fc89bd9b4d5a3ce7f171e7af914d9d8c98
serdigital64 <serdigital64@gmail.com>
2022-06-20 18:23:47 -0300

    feat: optimize code

--- src/bl64-ans-setup.bash
+++ src/bl64-ans-setup.bash
@@ -1,7 +1,7 @@
 #######################################
 # BashLib64 / Module / Setup / Interact with Ansible CLI
 #
-# Version: 1.2.0
+# Version: 1.3.0
 #######################################
 
 #######################################
@@ -11,7 +11,7 @@
 # * Check for core commands, fail if not available
 #
 # Arguments:
-#   $1: (optional) Full path to the ansible command
+#   $1: (optional) Full path to the ansible bin path where ansible commands are (ansible, ansible-playbook, ansible-galaxy)
 # Outputs:
 #   STDOUT: None
 #   STDERR: None
@@ -21,18 +21,18 @@
 #######################################
 function bl64_ans_setup() {
   bl64_dbg_lib_show_function "$@"
-  local cmd_ansible="${1:-}"
+  local ansible_bin="${1:-${BL64_LIB_DEFAULT}}"
 
-  # Use provided path if requested
-  if [[ -n "$cmd_ansible" ]]; then
-    BL64_ANS_CMD_ANSIBLE="$cmd_ansible"
-    BL64_ANS_CMD_ANSIBLE_GALAXY="${cmd_ansible}-galaxy"
-    BL64_ANS_CMD_ANSIBLE_PLAYBOOK="${cmd_ansible}-playbook"
+  if [[ "$ansible_bin" != "$BL64_LIB_DEFAULT" ]]; then
+    bl64_check_directory "$ansible_bin" ||
+    return $?
   fi
 
-  bl64_ans_set_command &&
+  bl64_ans_set_command "$ansible_bin" &&
     bl64_ans_set_options &&
     bl64_check_command "$BL64_ANS_CMD_ANSIBLE" &&
+    bl64_check_command "$BL64_ANS_CMD_ANSIBLE_GALAXY" &&
+    bl64_check_command "$BL64_ANS_CMD_ANSIBLE_PLAYBOOK" &&
     BL64_ANS_MODULE="$BL64_LIB_VAR_ON"
 
 }
@@ -42,7 +42,6 @@ function bl64_ans_setup() {
 #
 # * If no values are providedprovied, try to detect commands looking for common paths
 # * Commands are exported as variables with full path
-# * The caller function is responsible for checking that the target command is present (installed)
 #
 # Arguments:
 #   None
@@ -54,52 +53,39 @@ function bl64_ans_setup() {
 #######################################
 function bl64_ans_set_command() {
   bl64_dbg_lib_show_function
-  local ansible_root=''
+  local ansible_bin="$1"
 
-  if [[ -z "$BL64_ANS_CMD_ANSIBLE" ]]; then
+  if [[ "$ansible_bin" == "$BL64_LIB_DEFAULT" ]]; then
     if [[ -n "$BL64_PY_VENV_PATH" && -x "${BL64_PY_VENV_PATH}/bin/ansible" ]]; then
-      ansible_root="${BL64_PY_VENV_PATH}/bin"
+      ansible_bin="${BL64_PY_VENV_PATH}/bin"
     elif [[ -n "$HOME" && -x "${HOME}/.local/bin/ansible" ]]; then
-      ansible_root="${HOME}/.local/bin"
+      ansible_bin="${HOME}/.local/bin"
     elif [[ -x '/usr/local/bin/ansible' ]]; then
-      ansible_root='/usr/local/bin'
+      ansible_bin='/usr/local/bin'
     elif [[ -x '/home/linuxbrew/.linuxbrew/bin/ansible' ]]; then
-      ansible_root='/home/linuxbrew/.linuxbrew/bin'
+      ansible_bin='/home/linuxbrew/.linuxbrew/bin'
     elif [[ -x '/opt/homebrew/bin/ansible' ]]; then
-      ansible_root='/opt/homebrew/bin'
+      ansible_bin='/opt/homebrew/bin'
     elif [[ -x '/opt/ansible/bin/ansible' ]]; then
-      ansible_root='/opt/ansible/bin'
+      ansible_bin='/opt/ansible/bin'
     elif [[ -x '/usr/bin/ansible' ]]; then
-      ansible_root='/usr/bin'
+      ansible_bin='/usr/bin'
     fi
-    if [[ -n "$ansible_root" ]]; then
-      BL64_ANS_CMD_ANSIBLE="${ansible_root}/ansible"
-    else
-      BL64_ANS_CMD_ANSIBLE="$BL64_LIB_UNAVAILABLE"
-    fi
+  else
+    [[ ! -x "${ansible_bin}/ansible" ]] && ansible_bin="$BL64_LIB_DEFAULT"
   fi
 
-  if [[ -z "$BL64_ANS_CMD_ANSIBLE_GALAXY" ]]; then
-    if [[ -n "$ansible_root" && -x "${ansible_root}/ansible-galaxy" ]]; then
-      BL64_ANS_CMD_ANSIBLE_GALAXY="${ansible_root}/ansible-galaxy"
-    else
-      BL64_ANS_CMD_ANSIBLE_GALAXY="$BL64_LIB_UNAVAILABLE"
-    fi
-  fi
-
-  if [[ -z "$BL64_ANS_CMD_ANSIBLE_PLAYBOOK" ]]; then
-    if [[ -n "$ansible_root" && -x "${ansible_root}/ansible-playbook" ]]; then
-      BL64_ANS_CMD_ANSIBLE_PLAYBOOK="${ansible_root}/ansible-playbook"
-    else
-      BL64_ANS_CMD_ANSIBLE_PLAYBOOK="$BL64_LIB_UNAVAILABLE"
-    fi
+  if [[ "$ansible_bin" != "$BL64_LIB_DEFAULT" ]]; then
+    [[ -x "${ansible_bin}/ansible" ]] && BL64_ANS_CMD_ANSIBLE="${ansible_bin}/ansible"
+    [[ -x "${ansible_bin}/ansible-galaxy" ]] && BL64_ANS_CMD_ANSIBLE_GALAXY="${ansible_bin}/ansible-galaxy"
+    [[ -x "${ansible_bin}/ansible-playbook" ]] && BL64_ANS_CMD_ANSIBLE_PLAYBOOK="${ansible_bin}/ansible-playbook"
   fi
 
   # Publish common paths
   BL64_ANS_PATH_USR_ANSIBLE="${HOME}/.ansible"
   BL64_ANS_PATH_USR_COLLECTIONS="${BL64_ANS_PATH_USR_ANSIBLE}/collections/ansible_collections"
 
-  return 0
+  bl64_dbg_lib_show_vars 'BL64_ANS_CMD_ANSIBLE' 'BL64_ANS_CMD_ANSIBLE_GALAXY' 'BL64_ANS_CMD_ANSIBLE_PLAYBOOK' 'BL64_ANS_PATH_USR_ANSIBLE' 'BL64_ANS_PATH_USR_COLLECTIONS'
 }
 
 #######################################
