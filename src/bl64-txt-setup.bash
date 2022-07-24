004dd0591a9a2fb961e6c89fad8968b6858a4d11
serdigital64 <serdigital64@gmail.com>
2022-07-24 18:20:46 -0300

    feat: add grep flags

--- src/bl64-txt-setup.bash
+++ src/bl64-txt-setup.bash
@@ -1,7 +1,7 @@
 #######################################
 # BashLib64 / Module / Setup / Manipulate text files content
 #
-# Version: 1.3.0
+# Version: 1.4.0
 #######################################
 
 #######################################
@@ -22,6 +22,7 @@ function bl64_txt_setup() {
   bl64_dbg_lib_show_function
 
   bl64_txt_set_command &&
+    bl64_txt_set_options &&
     BL64_TXT_MODULE="$BL64_LIB_VAR_ON"
 
 }
@@ -88,4 +89,49 @@ function bl64_txt_set_command() {
     ;;
   *) bl64_check_alert_unsupported ;;
   esac
+}
+
+#######################################
+# Create command sets for common options
+#
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: None
+#   STDERR: None
+# Returns:
+#   0: always ok
+#######################################
+function bl64_txt_set_options() {
+  bl64_dbg_lib_show_function
+
+  # shellcheck disable=SC2034
+  case "$BL64_OS_DISTRO" in
+  ${BL64_OS_UB}-* | ${BL64_OS_DEB}-*)
+    BL64_TXT_SET_GREP_ERE='-E'
+    BL64_TXT_SET_GREP_INVERT='-v'
+    BL64_TXT_SET_GREP_NO_CASE='-i'
+    BL64_TXT_SET_GREP_SHOW_FILE_ONLY='-l'
+    ;;
+  ${BL64_OS_FD}-* | ${BL64_OS_CNT}-* | ${BL64_OS_RHEL}-* | ${BL64_OS_ALM}-* | ${BL64_OS_OL}-* | ${BL64_OS_RCK}-*)
+    BL64_TXT_SET_GREP_ERE='-E'
+    BL64_TXT_SET_GREP_INVERT='-v'
+    BL64_TXT_SET_GREP_NO_CASE='-i'
+    BL64_TXT_SET_GREP_SHOW_FILE_ONLY='-l'
+    ;;
+  ${BL64_OS_ALP}-*)
+    BL64_TXT_SET_GREP_ERE='-E'
+    BL64_TXT_SET_GREP_INVERT='-v'
+    BL64_TXT_SET_GREP_NO_CASE='-i'
+    BL64_TXT_SET_GREP_SHOW_FILE_ONLY='-l'
+    ;;
+  ${BL64_OS_MCOS}-*)
+    BL64_TXT_SET_GREP_ERE='-E'
+    BL64_TXT_SET_GREP_INVERT='-v'
+    BL64_TXT_SET_GREP_NO_CASE='-i'
+    BL64_TXT_SET_GREP_SHOW_FILE_ONLY='-l'
+    ;;
+  *) bl64_check_alert_unsupported ;;
+  esac
+
 }
