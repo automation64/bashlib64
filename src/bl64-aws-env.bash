42aa54b6cf9239b6a0034d93fd5b1a05742e0a10
serdigital64 <serdigital64@gmail.com>
2022-07-24 18:27:27 -0300

    feat: add sso, sts, profile support

--- src/bl64-aws-env.bash
+++ src/bl64-aws-env.bash
@@ -1,10 +1,32 @@
 #######################################
 # BashLib64 / Module / Globals / Interact with AWS
 #
-# Version: 1.0.0
+# Version: 1.1.0
 #######################################
 
 # Optional module. Not enabled by default
 export BL64_AWS_MODULE="$BL64_LIB_VAR_OFF"
 
 export BL64_AWS_CMD_AWS="$BL64_LIB_UNAVAILABLE"
+
+export BL64_AWS_SUFFIX_TOKEN='json'
+export BL64_AWS_SUFFIX_HOME='.aws'
+export BL64_AWS_SUFFIX_CACHE='sso/cache'
+export BL64_AWS_SUFFIX_CONFIG='cfg'
+export BL64_AWS_SUFFIX_CREDENTIALS='secret'
+
+export BL64_AWS_CLI_MODE='0700'
+export BL64_AWS_CLI_HOME=''
+export BL64_AWS_CLI_CONFIG=''
+export BL64_AWS_CLI_CREDENTIALS=''
+export BL64_AWS_CLI_TOKEN=''
+
+export BL64_AWS_SET_FORMAT_JSON=''
+export BL64_AWS_SET_FORMAT_TEXT=''
+export BL64_AWS_SET_FORMAT_TABLE=''
+export BL64_AWS_SET_FORMAT_YAML=''
+export BL64_AWS_SET_FORMAT_STREAM=''
+export BL64_AWS_SET_DEBUG=''
+export BL64_AWS_SET_OUPUT_NO_PAGER=''
+
+export BL64_AWS_TXT_TOKEN_NOT_FOUND='unable to locate temporary access token file'
