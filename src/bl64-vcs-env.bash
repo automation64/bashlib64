75733c68735d7c23d02e98be728509316ad4ceca
serdigital64 <serdigital64@gmail.com>
2022-07-24 18:10:05 -0300

    refact: use blank to reset values

--- src/bl64-vcs-env.bash
+++ src/bl64-vcs-env.bash
@@ -1,7 +1,7 @@
 #######################################
 # BashLib64 / Module / Globals / Manage Version Control System
 #
-# Version: 1.6.0
+# Version: 1.7.0
 #######################################
 
 export BL64_VCS_MODULE="$BL64_LIB_VAR_OFF"
@@ -14,9 +14,3 @@ export BL64_VCS_SET_GIT_NO_PAGER=''
 export BL64_VCS_SET_GIT_QUIET=''
 
 export _BL64_VCS_TXT_CLONE_REPO='clone single branch from GIT repository'
-
-# External commands variables
-export GIT_TRACE
-export GIT_CONFIG_NOSYSTEM
-export GIT_AUTHOR_EMAIL
-export GIT_AUTHOR_NAME
