fe5c2af75a46ed04ce277bbdb627be3cfe5e3dd5
serdigital64 <serdigital64@gmail.com>
2022-07-31 16:50:58 -0300

    feat: promote 4.0.0

--- bashlib64.bash
+++ bashlib64.bash
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 #######################################
-# BashLib64
+# BashLib64 / Bash automation librbary
 #
 # Author: serdigital64 (https://github.com/serdigital64)
-# License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: https://github.com/serdigital64/bashlib64
-# Version: 3.5.0
+# Version: 4.0.0
+#
+# Copyright 2022 SerDigital64@gmail.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 #######################################
 
 # Do not inherit aliases and commands
