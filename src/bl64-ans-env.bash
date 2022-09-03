71b2bff47d1600c43f0ead78030e7a775f97f30e
serdigital64 <serdigital64@gmail.com>
2022-09-03 10:05:31 -0300

    feat: add user config

--- src/bl64-ans-env.bash
+++ src/bl64-ans-env.bash
@@ -1,11 +1,15 @@
 #######################################
 # BashLib64 / Module / Globals / Interact with Ansible CLI
 #
-# Version: 1.4.0
+# Version: 1.5.0
 #######################################
 
 # Optional module. Not enabled by default
 export BL64_ANS_MODULE="$BL64_LIB_VAR_OFF"
+
+export BL64_ANS_ENV_IGNORE=''
+
+export BL64_ANS_VERSION_CORE=''
 
 export BL64_ANS_CMD_ANSIBLE="$BL64_LIB_UNAVAILABLE"
 export BL64_ANS_CMD_ANSIBLE_PLAYBOOK="$BL64_LIB_UNAVAILABLE"
@@ -13,6 +17,7 @@ export BL64_ANS_CMD_ANSIBLE_GALAXY="$BL64_LIB_UNAVAILABLE"
 
 export BL64_ANS_PATH_USR_ANSIBLE=''
 export BL64_ANS_PATH_USR_COLLECTIONS=''
+export BL64_ANS_PATH_USR_CONFIG=''
 
 export BL64_ANS_SET_VERBOSE=''
 export BL64_ANS_SET_DIFF=''
--- src/bl64-ans-setup.bash
+++ src/bl64-ans-setup.bash
@@ -1,7 +1,7 @@
 #######################################
 # BashLib64 / Module / Setup / Interact with Ansible CLI
 #
-# Version: 1.3.0
+# Version: 1.4.0
 #######################################
 
 #######################################
@@ -12,6 +12,8 @@
 #
 # Arguments:
 #   $1: (optional) Full path where commands are
+#   $2: (optional) Full path to the ansible configuration file
+#   $3: (optional) Ignore inherited shell environment? Default: BL64_LIB_VAR_ON
 # Outputs:
 #   STDOUT: None
 #   STDERR: None
@@ -23,19 +25,22 @@
 function bl64_ans_setup() {
   bl64_dbg_lib_show_function "$@"
   local ansible_bin="${1:-${BL64_LIB_DEFAULT}}"
-
-  if [[ "$ansible_bin" != "$BL64_LIB_DEFAULT" ]]; then
-    bl64_check_directory "$ansible_bin" ||
-    return $?
-  fi
+  local ansible_config="${2:-${BL64_LIB_DEFAULT}}"
+  local env_ignore="${3:-${BL64_LIB_VAR_ON}}"
 
   bl64_ans_set_command "$ansible_bin" &&
-    bl64_ans_set_options &&
     bl64_check_command "$BL64_ANS_CMD_ANSIBLE" &&
     bl64_check_command "$BL64_ANS_CMD_ANSIBLE_GALAXY" &&
     bl64_check_command "$BL64_ANS_CMD_ANSIBLE_PLAYBOOK" &&
-    BL64_ANS_MODULE="$BL64_LIB_VAR_ON"
+    bl64_ans_set_paths "$ansible_config" &&
+    bl64_ans_set_options &&
+    bl64_ans_set_version ||
+    return $?
 
+  BL64_ANS_MODULE="$BL64_LIB_VAR_ON"
+  BL64_ANS_ENV_IGNORE="$env_ignore"
+
+  return 0
 }
 
 #######################################
@@ -54,7 +59,7 @@ function bl64_ans_setup() {
 #######################################
 function bl64_ans_set_command() {
   bl64_dbg_lib_show_function
-  local ansible_bin="${1:-${BL64_LIB_DEFAULT}}"
+  local ansible_bin="$1"
 
   if [[ "$ansible_bin" == "$BL64_LIB_DEFAULT" ]]; then
     if [[ -n "$BL64_PY_VENV_PATH" && -x "${BL64_PY_VENV_PATH}/bin/ansible" ]]; then
@@ -72,8 +77,6 @@ function bl64_ans_set_command() {
     elif [[ -x '/usr/bin/ansible' ]]; then
       ansible_bin='/usr/bin'
     fi
-  else
-    [[ ! -x "${ansible_bin}/ansible" ]] && ansible_bin="$BL64_LIB_DEFAULT"
   fi
 
   if [[ "$ansible_bin" != "$BL64_LIB_DEFAULT" ]]; then
@@ -82,11 +85,8 @@ function bl64_ans_set_command() {
     [[ -x "${ansible_bin}/ansible-playbook" ]] && BL64_ANS_CMD_ANSIBLE_PLAYBOOK="${ansible_bin}/ansible-playbook"
   fi
 
-  # Publish common paths
-  BL64_ANS_PATH_USR_ANSIBLE="${HOME}/.ansible"
-  BL64_ANS_PATH_USR_COLLECTIONS="${BL64_ANS_PATH_USR_ANSIBLE}/collections/ansible_collections"
-
-  bl64_dbg_lib_show_vars 'BL64_ANS_CMD_ANSIBLE' 'BL64_ANS_CMD_ANSIBLE_GALAXY' 'BL64_ANS_CMD_ANSIBLE_PLAYBOOK' 'BL64_ANS_PATH_USR_ANSIBLE' 'BL64_ANS_PATH_USR_COLLECTIONS'
+  bl64_dbg_lib_show_vars 'BL64_ANS_CMD_ANSIBLE' 'BL64_ANS_CMD_ANSIBLE_GALAXY' 'BL64_ANS_CMD_ANSIBLE_PLAYBOOK'
+  return 0
 }
 
 #######################################
@@ -106,4 +106,83 @@ function bl64_ans_set_options() {
   BL64_ANS_SET_VERBOSE='-v'
   BL64_ANS_SET_DIFF='--diff'
   BL64_ANS_SET_DEBUG='-vvvvv'
+}
+
+#######################################
+# Set and prepare module paths
+#
+# * Global paths only
+# * If preparation fails the whole module fails
+#
+# Arguments:
+#   $1: path to ansible_config
+#   $2: path to ansible collections
+#   $3: path to ansible workdir
+# Outputs:
+#   STDOUT: None
+#   STDERR: check errors
+# Returns:
+#   0: paths prepared ok
+#   >0: failed to prepare paths
+#######################################
+function bl64_ans_set_paths() {
+  bl64_dbg_lib_show_function
+  local config="${1:-${BL64_LIB_DEFAULT}}"
+  local collections="${2:-${BL64_LIB_DEFAULT}}"
+  local ansible="${3:-${BL64_LIB_DEFAULT}}"
+
+  if [[ "$config" == "$BL64_LIB_DEFAULT" ]]; then
+    BL64_ANS_PATH_USR_CONFIG=''
+  else
+    bl64_check_file "$config" || return $?
+    BL64_ANS_PATH_USR_CONFIG="$config"
+  fi
+
+  if [[ "$ansible" == "$BL64_LIB_DEFAULT" ]]; then
+    BL64_ANS_PATH_USR_ANSIBLE="${HOME}/.ansible"
+  else
+    BL64_ANS_PATH_USR_ANSIBLE="$ansible"
+  fi
+
+  if [[ "$collections" == "$BL64_LIB_DEFAULT" ]]; then
+    BL64_ANS_PATH_USR_COLLECTIONS="${BL64_ANS_PATH_USR_ANSIBLE}/collections/ansible_collections"
+  else
+    bl64_check_directory "$collections" || return $?
+    BL64_ANS_PATH_USR_COLLECTIONS="$ansible"
+  fi
+
+  bl64_dbg_lib_show_vars 'BL64_ANS_PATH_USR_CONFIG' 'BL64_ANS_PATH_USR_ANSIBLE' 'BL64_ANS_PATH_USR_COLLECTIONS'
+  return 0
+}
+
+#######################################
+# Identify and set module components versions
+#
+# * Version information is stored in module global variables
+#
+# Arguments:
+#   None
+# Outputs:
+#   STDOUT: None
+#   STDERR: command errors
+# Returns:
+#   0: version set ok
+#   >0: command error
+#######################################
+function bl64_ans_set_version() {
+  bl64_dbg_lib_show_function
+  local version=''
+
+  bl64_dbg_lib_show_info "run ansible to obtain ansible-core version"
+  version="$("$BL64_ANS_CMD_ANSIBLE" --version | bl64_txt_run_awk '/^ansible..core.*$/ { gsub( /\[|\]/, "" ); print $3 }')"
+
+  if [[ -n "$version" ]]; then
+    BL64_ANS_VERSION_CORE="$version"
+  else
+    # shellcheck disable=SC2086
+    return $BL64_LIB_ERROR_APP_INCOMPATIBLE
+  fi
+
+  bl64_dbg_lib_show_vars 'BL64_ANS_VERSION_CORE'
+  return 0
 }
