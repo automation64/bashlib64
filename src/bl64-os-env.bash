20840bc07747244d8cfb488e1d4d462407127d62
serdigital64 <serdigital64@gmail.com>
2023-02-02 08:01:23 -0300

    refact: removed obsolete

--- src/bl64-os-env.bash
+++ src/bl64-os-env.bash
@@ -1,7 +1,7 @@
 #######################################
 # BashLib64 / Module / Globals / OS / Identify OS attributes and provide command aliases
 #
-# Version: 1.14.0
+# Version: 1.15.0
 #######################################
 
 export BL64_OS_MODULE="$BL64_LIB_VAR_OFF"
@@ -18,30 +18,27 @@ export BL64_OS_CMD_BASH=''
 
 export BL64_OS_ALIAS_ID_USER=''
 
-# OS tags. Uppercase version of os-release id
-export BL64_OS_TAGS='ALM ALP AMZ CNT DEB FD MCOS OL RCK RHEL UB'; export BL64_OS_TAGS
-
-#   * ALM   -> AlmaLinux
-export BL64_OS_ALM='ALMALINUX'; export BL64_OS_ALM
-#   * ALP   -> Alpine Linux
-export BL64_OS_ALP='ALPINE'; export BL64_OS_ALP
-#   * AMZ   -> Amazon Linux
-export BL64_OS_AMZ='AMZN'; export BL64_OS_AMZ
-#   * CNT   -> CentOS
-export BL64_OS_CNT='CENTOS'; export BL64_OS_CNT
-#   * DEB   -> Debian
-export BL64_OS_DEB='DEBIAN'; export BL64_OS_DEB
-#   * FD    -> Fedora
-export BL64_OS_FD='FEDORA'; export BL64_OS_FD
-#   * MCOS  -> MacOS (Darwin)
-export BL64_OS_MCOS='DARWIN'; export BL64_OS_MCOS
-#   * OL    -> OracleLinux
-export BL64_OS_OL='OL'; export BL64_OS_OL
-#   * RCK  -> Rocky Linux
-export BL64_OS_RCK='ROCKY'; export BL64_OS_RCK
-#   * RHEL  -> RedHat Enterprise Linux
-export BL64_OS_RHEL='RHEL'; export BL64_OS_RHEL
-#   * UB    -> Ubuntu
-export BL64_OS_UB='UBUNTU'; export BL64_OS_UB
-#   * UNK    -> Unknown OS
-export BL64_OS_UNK='UNKNOWN'; export BL64_OS_UNK
+# ALM -> AlmaLinux
+export BL64_OS_ALM='ALMALINUX'
+# ALP -> Alpine Linux
+export BL64_OS_ALP='ALPINE'
+# AMZ -> Amazon Linux
+export BL64_OS_AMZ='AMZN'
+# CNT -> CentOS
+export BL64_OS_CNT='CENTOS'
+# DEB -> Debian
+export BL64_OS_DEB='DEBIAN'
+# FD  -> Fedora
+export BL64_OS_FD='FEDORA'
+# MCOS-> MacOS (Darwin)
+export BL64_OS_MCOS='DARWIN'
+# OL  -> OracleLinux
+export BL64_OS_OL='OL'
+# RCK-> Rocky Linux
+export BL64_OS_RCK='ROCKY'
+# RHEL-> RedHat Enterprise Linux
+export BL64_OS_RHEL='RHEL'
+# UB  -> Ubuntu
+export BL64_OS_UB='UBUNTU'
+# UNK  -> Unknown OS
+export BL64_OS_UNK='UNKNOWN'
