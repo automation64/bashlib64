0d2b70cdb5e908a7e7b4431e75d183b130b33feb
serdigital64 <serdigital64@gmail.com>
2023-03-19 19:12:23 -0300

    feat: added 1.26 support

--- src/bl64-k8s-setup.bash
+++ src/bl64-k8s-setup.bash
@@ -1,7 +1,7 @@
 #######################################
 # BashLib64 / Module / Setup / Interact with Kubernetes
 #
-# Version: 1.3.0
+# Version: 1.4.0
 #######################################
 
 #######################################
@@ -90,7 +90,7 @@ function _bl64_k8s_set_options() {
   bl64_dbg_lib_show_function
 
   case "$BL64_K8S_VERSION_KUBECTL" in
-  1.22 | 1.23 | 1.24 | 1.25 )
+  1.22 | 1.23 | 1.24 | 1.25 | 1.26)
     BL64_K8S_SET_VERBOSE_NONE='--v=0'
     BL64_K8S_SET_VERBOSE_NORMAL='--v=2'
     BL64_K8S_SET_VERBOSE_DEBUG='--v=4'
