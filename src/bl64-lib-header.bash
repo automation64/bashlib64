4e6097791b3cd420abed1846670caeae9df85757
Carola <carolademaria@gmail.com>
2023-03-29 14:32:41 -0300

    feact: add rockylinux9

--- src/bl64-lib-header.bash
+++ src/bl64-lib-header.bash
@@ -4,7 +4,7 @@
 #
 # Author: serdigital64 (https://github.com/serdigital64)
 # Repository: https://github.com/serdigital64/bashlib64
-# Version: 10.3.0
+# Version: 11.0.0
 #
 # Copyright 2022 SerDigital64@gmail.com
 #
