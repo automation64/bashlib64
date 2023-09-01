c50ccfb35bcb3c0794cdcc28a0fc54a7fcc3ef3b
serdigital64 <serdigital64@gmail.com>
2023-09-01 09:35:12 -0300

    refact: add error report

--- src/bl64-vcs-env.bash
+++ src/bl64-vcs-env.bash
@@ -2,7 +2,7 @@
 # BashLib64 / Module / Globals / Manage Version Control System
 #######################################
 
-export BL64_VCS_VERSION='1.13.0'
+export BL64_VCS_VERSION='1.14.0'
 
 export BL64_VCS_MODULE="$BL64_VAR_OFF"
 
@@ -11,8 +11,17 @@ export BL64_VCS_CMD_GIT=''
 export BL64_VCS_SET_GIT_NO_PAGER=''
 export BL64_VCS_SET_GIT_QUIET=''
 
+#
+# GitHub related parameters
+#
+
+# GitHub API FQDN
 export BL64_VCS_GITHUB_API_URL='https://api.github.com'
+# Target GitHub public API version
 export BL64_VCS_GITHUB_API_VERSION='2022-11-28'
+# Special tag for latest release
+export BL64_VCS_GITHUB_LATEST='latest'
 
 export _BL64_VCS_TXT_CLONE_REPO='clone single branch from GIT repository'
 export _BL64_VCS_TXT_GET_LATEST_RELEASE='get release tag from latest release'
+export _BL64_VCS_TXT_GET_LATEST_RELEASE_FAILED='unable to determine latest release'
