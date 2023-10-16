1b803b65605106e85e164951b10af7fa98b43a13
serdigital64 <serdigital64@gmail.com>
2023-10-16 13:04:59 -0300

    refact: optimize

--- src/bl64-hlm-env.bash
+++ src/bl64-hlm-env.bash
@@ -2,7 +2,7 @@
 # BashLib64 / Module / Globals / Interact with HLM
 #######################################
 
-export BL64_HLM_VERSION='1.2.1'
+export BL64_HLM_VERSION='1.3.0'
 
 # Optional module. Not enabled by default
 export BL64_HLM_MODULE="$BL64_VAR_OFF"
@@ -15,3 +15,7 @@ export BL64_HLM_SET_OUTPUT_JSON=''
 export BL64_HLM_SET_OUTPUT_YAML=''
 
 export BL64_HLM_RUN_TIMEOUT=''
+
+export BL64_HLM_TXT_ADD_REPO='add Helm repository'
+export BL64_HLM_TXT_UPDATE_REPO='update Helm repository catalog'
+export BL64_HLM_TXT_DEPLOY_CHART='deploy helm chart'
