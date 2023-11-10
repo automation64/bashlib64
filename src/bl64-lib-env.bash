d963eac5fa4bdd924ab7ef6efc02120b790d6762
serdigital64 <serdigital64@gmail.com>
2023-11-10 19:19:57 -0300

    doc: update version

--- src/bl64-lib-env.bash
+++ src/bl64-lib-env.bash
@@ -2,7 +2,7 @@
 # BashLib64 / Module / Globals / Setup script run-time environment
 #######################################
 
-export BL64_VERSION='17.0.0'
+export BL64_VERSION='17.1.0'
 
 #
 # Imported shell standard variables
