bc3a735bbe6a2b23a5abc0e8832c989c91dd45f4
serdigital64 <serdigital64@gmail.com>
2023-12-02 21:02:21 +0000

    fix: add missing check

--- src/bl64-xsv.bash
+++ src/bl64-xsv.bash
@@ -118,7 +118,8 @@ function bl64_xsv_search_records() {
 function bl64_xsv_run_jq() {
   bl64_dbg_lib_show_function "$@"
 
-  bl64_check_module 'BL64_XSV_MODULE' &&
+  bl64_check_parameters_none "$#" &&
+    bl64_check_module 'BL64_XSV_MODULE' &&
     bl64_check_command "$BL64_XSV_CMD_JQ" ||
     return $?
 
@@ -145,7 +146,8 @@ function bl64_xsv_run_jq() {
 function bl64_xsv_run_yq() {
   bl64_dbg_lib_show_function "$@"
 
-  bl64_check_module 'BL64_XSV_MODULE' &&
+  bl64_check_parameters_none "$#" &&
+    bl64_check_module 'BL64_XSV_MODULE' &&
     bl64_check_command "$BL64_XSV_CMD_YQ" "$BL64_VAR_DEFAULT" 'yq' ||
     return $?
 
