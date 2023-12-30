f98a8eb5e860f868c742618f96826b116a13bdf7
serdigital64 <serdigital64@gmail.com>
2023-12-30 18:44:47 +0000

    refact: normalize var names

--- src/bl64-fs-env.bash
+++ src/bl64-fs-env.bash
@@ -2,7 +2,7 @@
 # BashLib64 / Module / Globals / Manage local filesystem
 #######################################
 
-export BL64_FS_VERSION='4.7.1'
+export BL64_FS_VERSION='4.8.0'
 
 export BL64_FS_MODULE='0'
 
