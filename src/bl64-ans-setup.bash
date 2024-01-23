e6b19ab2125f13cc3d02692b76f3a9899149ecd8
serdigital64 <serdigital64@gmail.com>
2024-01-23 11:58:31 -0300

    fix: add missing module req

--- src/bl64-ans-setup.bash
+++ src/bl64-ans-setup.bash
@@ -34,6 +34,7 @@ function bl64_ans_setup() {
     bl64_dbg_lib_show_function &&
     bl64_lib_module_imported 'BL64_MSG_MODULE' &&
     bl64_lib_module_imported 'BL64_TXT_MODULE' &&
+    bl64_lib_module_imported 'BL64_PY_MODULE' &&
     _bl64_ans_set_command "$ansible_bin" &&
     bl64_check_command "$BL64_ANS_CMD_ANSIBLE" &&
     bl64_check_command "$BL64_ANS_CMD_ANSIBLE_GALAXY" &&
