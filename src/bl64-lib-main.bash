b62caed3e48ae5993cc93c103fed3144f1489f6f
serdigital64 <serdigital64@gmail.com>
2024-03-01 18:25:40 +0000

    fix: add missing OS

--- src/bl64-lib-main.bash
+++ src/bl64-lib-main.bash
@@ -61,7 +61,7 @@ if [[ "${BL64_OS_MODULE:-}" == "$BL64_VAR_ON" ]]; then
     "${BL64_OS_ALP}-3" \
     "${BL64_OS_AMZ}-2023" \
     "${BL64_OS_CNT}-7" "${BL64_OS_CNT}-8" "${BL64_OS_CNT}-9" \
-    "${BL64_OS_DEB}-9" "${BL64_OS_DEB}-10" "${BL64_OS_DEB}-11" \
+    "${BL64_OS_DEB}-9" "${BL64_OS_DEB}-10" "${BL64_OS_DEB}-11" "${BL64_OS_DEB}-12" \
     "${BL64_OS_FD}-33" "${BL64_OS_FD}-34" "${BL64_OS_FD}-35" "${BL64_OS_FD}-36" "${BL64_OS_FD}-37" "${BL64_OS_FD}-38" "${BL64_OS_FD}-39" \
     "${BL64_OS_OL}-7" "${BL64_OS_OL}-8" "${BL64_OS_OL}-9" \
     "${BL64_OS_RCK}-8" "${BL64_OS_RCK}-9" \
