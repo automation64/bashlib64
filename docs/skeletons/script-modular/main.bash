e4a5f3861c964bf23a6847bc7179dc240f0a5a92
serdigital64 <serdigital64@gmail.com>
2024-03-11 14:59:47 +0100

    doc: remove mkdocs

---
+++ docs/skeletons/script-modular/main.bash
@@ -0,0 +1,9 @@
+#
+###[ embedded-bashlib64-end ]#####################
+#
+
+#
+# Main
+#
+
+# X_MAIN_PLACEHOLDER_X
---
+++ docs/snippets/globals-os/version.bash
@@ -0,0 +1,10 @@
+"${BL64_OS_ALM}-8" "${BL64_OS_ALM}-9" \
+"${BL64_OS_ALP}-3" \
+"${BL64_OS_CNT}-7" "${BL64_OS_CNT}-8" "${BL64_OS_CNT}-9" \
+"${BL64_OS_DEB}-9" "${BL64_OS_DEB}-10" "${BL64_OS_DEB}-11" \
+"${BL64_OS_FD}-33" "${BL64_OS_FD}-34" "${BL64_OS_FD}-35" "${BL64_OS_FD}-36" "${BL64_OS_FD}-37" "${BL64_OS_FD}-38" "${BL64_OS_FD}-39" \
+"${BL64_OS_OL}-7" "${BL64_OS_OL}-8" "${BL64_OS_OL}-9" \
+"${BL64_OS_RCK}-8" "${BL64_OS_RCK}-9" \
+"${BL64_OS_RHEL}-8" "${BL64_OS_RHEL}-9" \
+"${BL64_OS_SLES}-15" \
+"${BL64_OS_UB}-18" "${BL64_OS_UB}-20" "${BL64_OS_UB}-21" "${BL64_OS_UB}-22" "${BL64_OS_UB}-23"
