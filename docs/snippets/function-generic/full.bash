8b1f739ac2882eacb5b81ddfa321d336d336cc42
serdigital64 <serdigital64@gmail.com>
2024-03-28 10:49:38 +0000

    doc: update templates

---
+++ docs/snippets/function-generic/full.bash
@@ -0,0 +1,15 @@
+#######################################
+# X_DESCRIPTION_X
+#
+# Arguments:
+#   $1: X_PARAMETER_DESCRIPTION_X
+# Outputs:
+#   STDOUT: X_OUTPUT_DESCRIPTION_X
+#   STDERR: X_ERROR_DESCRIPTION_X
+# Returns:
+#   X_EXITCODE_X: X_EXITCODE_DESCRIPTION_X
+#######################################
+function bl64_X_MODULE_X_X_FUNCTION_X() {
+  bl64_dbg_lib_show_function "$@"
+  # X_FUNCTION_GENERIC_PLACEHOLDER_X
+}
