a162a873537a3d0d3b92530f24669b083ace6be9
serdigital64 <serdigital64@gmail.com>
2024-05-18 12:21:12 +0200

    feat: add env gen

--- src/bl64-bsh-env.bash
+++ src/bl64-bsh-env.bash
@@ -4,11 +4,12 @@
 
 # shellcheck disable=SC2034
 {
-  declare BL64_BSH_VERSION='3.0.0'
+  declare BL64_BSH_VERSION='3.1.0'
 
   declare BL64_BSH_MODULE='0'
 
   declare BL64_BSH_VERSION_BASH=''
+  declare BL64_BSH_ENV_STORE='.env.d'
 
   declare _BL64_BSH_TXT_IMPORT_YAML='convert and import shell variables from YAML file'
 }
