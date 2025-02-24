19128021182c426750716911e42c8eebeb5563bb
SerDigital64 <serdigital64@gmail.com>
2025-02-24 06:28:06 +0000

    doc: update

---
+++ docs/skeletons/module-base/bl64-MODULE.bash
@@ -0,0 +1,25 @@
+# Template: v1.0.0
+#######################################
+# BashLib64 / Module / Functions / X_MODULE_OVERVIEW_X
+#######################################
+
+#
+# Deprecation aliases
+#
+# * Aliases to deprecated functions
+# * Needed to maintain compatibility up to N-2 versions
+#
+
+# X_FUNCTIONS_DEPRECATED_X
+
+#
+# Private functions
+#
+
+# X_FUNCTIONS_PRIVATE_X
+
+#
+# Public functions
+#
+
+# X_FUNCTIONS_PUBLIC_X
