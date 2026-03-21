c73cb3e7caba954bd1feaea3a412167ca25334e1
SerDigital64 <serdigital64@gmail.com>
2026-03-21 11:37:22 +0100

    refact: remove obsolete

--- src/bl64-gcp-env.bash
+++ src/bl64-gcp-env.bash
@@ -4,7 +4,7 @@
 
 # shellcheck disable=SC2034
 {
-  declare BL64_GCP_VERSION='3.0.1'
+  declare BL64_GCP_VERSION='3.0.2'
 
   declare BL64_GCP_MODULE='0'
 
@@ -12,10 +12,6 @@
 
   declare BL64_GCP_CONFIGURATION_NAME='bl64_gcp_configuration_private'
   declare BL64_GCP_CONFIGURATION_CREATED="$BL64_VAR_FALSE"
-
-  declare BL64_GCP_SET_FORMAT_YAML=''
-  declare BL64_GCP_SET_FORMAT_TEXT=''
-  declare BL64_GCP_SET_FORMAT_JSON=''
 
   declare BL64_GCP_CLI_PROJECT=''
   declare BL64_GCP_CLI_IMPERSONATE_SA=''
